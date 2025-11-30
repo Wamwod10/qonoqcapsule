5327899287bb3c1c3949d07c01788f648d2b7f67
Wamwod10 <shamshodochilov160@gmail.com>
2025-11-30 21:38:42 +0500

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Capsules from "./components/capsules/Capsules";
+import Footer from "./components/footer/Footer";
 import Gallery from "./components/gallery/Gallery";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
@@ -14,6 +15,7 @@ function App() {
         <Qonoq />
         <Capsules />
         <Gallery />
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="container">
+            <div className="footer__box">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
